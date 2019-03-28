fe26398aeb95d8fd633c4bb8128a0cd47e31084a
Jong Woong To <jongwoongto@localhost.localdomain>
2019-03-28 12:56:33 +0900

    tidied up code

--- i3_tidy.py
+++ i3_tidy.py
@@ -7,22 +7,24 @@ import i3ipc
 def tidy_workspaces(i3):
 
     for workspace_index, workspace in enumerate(i3.get_tree().workspaces()):
-        
-        old_name = workspace.name 
+
+        old_name = workspace.name
 
         # If the workspace is in the wrong number assign it to the correct num
         if(workspace.num != workspace_index + 1):
             workspace.num = workspace_index + 1
 
         new_name = rename_workspace(workspace)
-        
-        #Command i3 to recorrect the workspaces
+
+        # Command i3 to recorrect the workspaces
         i3.command('rename workspace "%s" to "%s"' % (old_name, new_name))
+
 
 def rename_workspace(workspace):
     """Change the workspace name to reflect the correct workspace number"""
     workspace.name = re.sub('^\d*', str(workspace.num), workspace.name)
-    return workspace.name        
+    return workspace.name
+
 
 if __name__ == '__main__':
     i3 = i3ipc.Connection()
