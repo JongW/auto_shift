be8136d6863bd20bbe1ffa55912758e995703e5e
Jong Woong To <jongwoongto@localhost.localdomain>
2019-03-28 12:51:27 +0900

    tidied up code

--- i3_tidy.py
+++ i3_tidy.py
@@ -8,11 +8,15 @@ def tidy_workspaces(i3):
 
     for workspace_index, workspace in enumerate(i3.get_tree().workspaces()):
         
-        old_name = workspace.name
+        old_name = workspace.name 
+
+        # If the workspace is in the wrong number assign it to the correct num
         if(workspace.num != workspace_index + 1):
             workspace.num = workspace_index + 1
 
         new_name = rename_workspace(workspace)
+        
+        #Command i3 to recorrect the workspaces
         i3.command('rename workspace "%s" to "%s"' % (old_name, new_name))
 
 def rename_workspace(workspace):
